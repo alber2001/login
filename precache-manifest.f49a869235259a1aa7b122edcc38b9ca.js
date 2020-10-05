5486bb1ac15fddcc95307e3261a4d525a7bc27cd
Alber2001 <albertova_1989@hotmail.com>
2020-10-05 14:08:28 -0600

    Updates

---
+++ precache-manifest.f49a869235259a1aa7b122edcc38b9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd02379716fdb8141dbd86b798685b0",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "0daebc64da92c12f3b65",
+    "url": "/login/static/css/main.abd7cef3.chunk.css"
+  },
+  {
+    "revision": "5036778fddd45c0f8a10",
+    "url": "/login/static/js/2.42b16ede.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/login/static/js/2.42b16ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0daebc64da92c12f3b65",
+    "url": "/login/static/js/main.f7b85f6e.chunk.js"
+  },
+  {
+    "revision": "2a6ddebc308cbef80bfa",
+    "url": "/login/static/js/runtime-main.eced9b5c.js"
+  }
+]);
